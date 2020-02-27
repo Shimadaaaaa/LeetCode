21069d3f2af93539b11c9caa68a9fa3c9681ee11
Shimadaaaaa <13074163825@163.com>
2020-02-27 15:37:18 +0800

    delete 206.cpp

--- LinkedList/206.cpp
+++ LinkedList/206.cpp
@@ -9,16 +9,14 @@
 //头插法
 struct ListNode* reverseList(struct ListNode* head){
     if(head == NULL) return head;
-    struct ListNode *pre_node = (struct ListNode *)malloc(sizeof(struct ListNode));
-    pre_node->next = head;
-    struct ListNode *p = pre_node->next, *q;
-    pre_node->next = NULL;
+    struct ListNode pre_node;
+    struct ListNode *p = head, *q;
+    pre_node.next = NULL;
     while(p != NULL){
         q = p->next;
-        p->next = pre_node->next;
-        pre_node->next = p;
+        p->next = pre_node.next;
+        pre_node.next = p;
         p = q;
     }
-    return pre_node->next;
-    
+    return pre_node.next;  
 }
